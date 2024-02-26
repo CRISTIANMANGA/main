22eca0efb99b471dfc8d0e6de49c0e7eb939aadd
CRISTIANMANGA <160682767+CRISTIANMANGA@users.noreply.github.com>
2024-02-26 09:40:53 -0500

    Info

---
+++ main.py
@@ -0,0 +1,8 @@
+nombre = input("¿Como te llamas?:")
+edad = input("¿Que edad tienes?: ")
+altura = input ("¿Cual es tu altura?: ")
+direccion = input( "¿Cual es tu direeción?: ")
+Experiencia = input ("¿Tines alguna experiencia laboral? :")
+info = (nombre, edad, altura, direccion,Experiencia)
+
+print (info)
